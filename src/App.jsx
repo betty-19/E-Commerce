57ae3c7d15a2912d423b73e646fca69857e1a2f0
betty-19 <betelhemsintayehu10@gmail.com>
2025-07-17 04:23:55 +0100

    working on  Header

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@
 
 import { Routes,Route } from "react-router-dom"
 import SummerSale from "./components/SummerSale/SummerSale"
+import Header from "./components/Header/Header"
 
 function App() {
 
 
   return (
+    <>
+    <SummerSale/>
     <Routes>
-     <Route path='/' element={<SummerSale/>} />
+     <Route path='/' element={<Header/>} />
     </Routes>
+    </>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,18 +7,18 @@ const Header=()=>{
     return(
 <div className="headerWrapper">
     <p >Exclusive</p>
-    <div className="nav-items">
-        <ul>
-            <li>Home</li>
-            <li>Contact</li>
-            <li>About</li>
-            <li>Sign Up</li>
+    <div className="nav-items-div">
+        <ul className="nav-items">
+            <li className="nav-item">Home</li>
+            <li className="nav-item">Contact</li>
+            <li className="nav-item">About</li>
+            <li className="nav-item">Sign Up</li>
         </ul>
-        <div className="search">
+        
+    </div><div className="search">
             <input type="text" placeholder="What are you looking for?"/>
             <i></i>
         </div>
-    </div>
 </div>
     )
 }
